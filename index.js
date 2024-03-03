ced71e8b8d3296e0631fe2db4d4ac9b4cd6a2945
AdrianTerciado <terciadojano@gmail.com>
2024-03-03 18:48:18 +0100

    Terminado, incluída parte de bonus

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cardTemplate = function (country) {
 
 const countriesNode = document.getElementById("countries");
 
+
 fetch("https://restcountries.com/v3.1/all")
   /*   .then(function (response) {
       // fetch() returns a promise containing the response (a Response object).
@@ -30,11 +31,43 @@ fetch("https://restcountries.com/v3.1/all")
       }
     });*/
 
-  /* Este segundo then también podemos refactorizarlo con una función flecha y un foreach aunque el for each */
-   .then(countries => countries
-                          .sort((a, b) => (a.name.common).localeCompare(b.name.common))
-                          .forEach(country => countriesNode.innerHTML += cardTemplate(country))); 
+  /* Este segundo then también podemos refactorizarlo con una función flecha y un foreach aunque el for each no reduce mucho el código*/
+  .then(countries => countries
+    .sort((a, b) => (a.name.common).localeCompare(b.name.common))
+    .forEach(country => countriesNode.innerHTML += cardTemplate(country)));
 
 /* En el ejemplo salen los países ordenados alfabeticamente, tengo que mirar cómo hacerlo */
 
 /* Para cada país pintamos imagen con un id que se repite siempre pero los id deberían ser únicos. Podemos eliminar ese id y cambiarlo por un class pero tenemos que cambiar también el selector de CSS que le da estilo. */
+
+
+/* Bonus */
+
+// Cuando hacemos click en el input (el botón)
+document.querySelector("input").addEventListener("click", function () {
+
+  // Capturamos en una variable el valor del selector para despues comparar aunque podemos meterlo a capón pero es menos legible
+  
+  let seleccion = document.getElementById("continente").value;
+
+  // Si elegimos todos: mostramos lo que hemos hecho arriba, se quedaría igual.
+  if (seleccion == "All") {
+    countriesNode.innerHTML = "";
+    fetch("https://restcountries.com/v3.1/all")
+      .then(response => response.json())
+      .then(countries => countries
+        .sort((a, b) => (a.name.common).localeCompare(b.name.common))
+        .forEach(country => countriesNode.innerHTML += cardTemplate(country))
+      );
+  }
+  // Pero si no "All" es porque estamos eligiendo alguna region, entonces...
+  else {
+      countriesNode.innerHTML = "";
+      fetch(`https://restcountries.com/v3.1/region/${seleccion}`)
+        .then(response => response.json())
+        .then(countries => countries
+          .sort((a, b) => a.name.common.localeCompare(b.name.common))
+          .forEach(country =>
+            countriesNode.innerHTML += cardTemplate(country)))
+  }
+});
